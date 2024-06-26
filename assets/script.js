c4fdc196361bb9d05efeffc49369e62cc081ab7a
Ashlyn McGarry <166701777+ashlynmcgarry@users.noreply.github.com>
2024-06-26 16:15:35 +0930

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -6,26 +6,23 @@ const mainWeatherDiv = $(".main-weather-details");
 const form = $("form");
 const historyButtons = $("#search-history");
 
-let searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || []; //saving search history to local storage array (or starting a blank one if there is none)
+let searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || []; 
 
 function getMainWeather(city) {
-  //getting weather for main header card
   const cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=metric`;
 
   if (city === "") {
-    // If there is no input, alert and stop the function
     alert("Please enter a city name.");
     return;
   }
 
-  fetch(cityURL) //fetching API response
+  fetch(cityURL) 
     .then((response) => response.json())
     .then((data) => {
-      console.log(data); //logging the API response
+      console.log(data); 
 
-      mainWeatherDiv.empty(); //clearing out existing data from the div
+      mainWeatherDiv.empty(); 
 
-      //populating the main weather card
       const mainWeatherHTML = ` 
         <h2>${data.name}</h2>
         <h4>Temperature: ${data.main.temp}°C</h4>
@@ -33,35 +30,30 @@ function getMainWeather(city) {
         <h4>Humidity: ${data.main.humidity}%</h4>
         <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="weather-icon">`;
 
-      mainWeatherDiv.append(mainWeatherHTML); //appending to the HTML
+      mainWeatherDiv.append(mainWeatherHTML);
 
-      saveToSearchHistory(city); //saving to the search history
+      saveToSearchHistory(city);
 
-      // After fetching main weather, get lat and long data for forecast weather
       getForecastWeather(data.coord.lat, data.coord.lon);
     });
 }
 
 function getForecastWeather(lat, lon) {
-  //getting forecast weather for cards
   const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`;
 
-  fetch(forecastURL) //calling API utilising lat and lon data from above
+  fetch(forecastURL) 
     .then((response) => response.json())
     .then((data) => {
-      console.log(data); //logging this data
+      console.log(data); 
 
-      forecastCardsDiv.empty(); //emptying cards of existing data
+      forecastCardsDiv.empty(); 
 
       const filterDates = data.list.filter((forecast) => {
-        //filtering dates to only show information featuring '15:00:00', so once per day
         return forecast.dt_txt.includes("15:00:00");
       });
 
-      // Display multiple forecast cards
       filterDates.slice(0, 5).forEach((forecast) => {
         const forecastDate = new Date(forecast.dt_txt);
-        //pushing cards to HTML and converting the date to a string to be read
         const forecastCardHTML = `
           <div class="forecast-card">
             <h3>${forecastDate.toLocaleDateString()}</h3> 
@@ -72,45 +64,39 @@ function getForecastWeather(lat, lon) {
               forecast.weather[0].icon
             }.png" alt="weather-icon">
           </div>`;
-        forecastCardsDiv.append(forecastCardHTML); //appending the HTML
+        forecastCardsDiv.append(forecastCardHTML); 
       });
     });
 }
 
 function saveToSearchHistory(city) {
-  // Add city to search history
   searchHistory.push(city);
 
-  // Save search history array to local storage
   localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
   pushSearchHistory();
 }
 
 function pushSearchHistory() {
-  //pushing the search history to buttons
-  historyButtons.empty(); //clearing existing button data
+  historyButtons.empty(); 
 
   searchHistory.forEach((city) => {
-    //pushing search history array to buttons, using city name
     const $button = $(
       `<button type='button' class='btn btn-secondary'></button>`
-    ) //definig the button
-      .text(city) //telling the text to be city
+    ) 
+      .text(city) 
       .click(() => {
-        //saying on click perform getMainWeather function using this city
+        
         getMainWeather(city);
       });
-    historyButtons.append($button); //appending the buttons to the HTML
+    historyButtons.append($button); 
   });
 }
 
 pushSearchHistory();
 
-// Submit event listener for the form
 form.on("submit", function (event) {
   event.preventDefault();
 
-  //defining that city is the input value from the form
   let city = searchInput.val();
-  getMainWeather(city); //pushing that to the getMainWeather function
+  getMainWeather(city); 
 });
