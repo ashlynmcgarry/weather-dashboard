9d7d6023d0fa7505e8d9f8372009e7ce1db29a7f
Ashlyn McGarry <166701777+ashlynmcgarry@users.noreply.github.com>
2024-06-27 11:20:57 +0930

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,67 @@
 const APIKey = "f004268a18ec7830138895b0e31a3214";
-const searchButton = $(".search-button");
-const searchInput = $(".search-input");
+const searchInput = $('.search-input');
+const searchForm = $('form');
+const mainWeatherDetails = $('.main-weather-details');
 const forecastCardsDiv = $(".forecast-cards");
-const mainWeatherDiv = $(".main-weather-details");
-const form = $("form");
 const historyButtons = $("#search-history");
 
-let searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || []; 
+let searchHistory = JSON.parse(localStorage.getItem ("searchHistory")) || [];
 
-function getMainWeather(city) {
-  const cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=metric`;
+searchForm.on('submit', function(event) {
+    event.preventDefault();
 
-  if (city === "") {
-    alert("Please enter a city name.");
-    return;
-  }
+    let city = searchInput.val();
+    mainWeather(city);
+});
 
-  fetch(cityURL) 
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data); 
+function mainWeather(city) {
+    const mainWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=metric`;
 
-      mainWeatherDiv.empty(); 
+    fetch(mainWeatherAPI)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
 
-      const mainWeatherHTML = ` 
-        <h2>${data.name}</h2>
+    mainWeatherDetails.empty();
+
+    const mainWeatherHTML = `
+        <h2>Current Weather in ${data.name}</h2>
         <h4>Temperature: ${data.main.temp}°C</h4>
         <h4>Wind: ${data.wind.speed} km/h</h4>
         <h4>Humidity: ${data.main.humidity}%</h4>
         <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="weather-icon">`;
 
-      mainWeatherDiv.append(mainWeatherHTML);
+    mainWeatherDetails.append(mainWeatherHTML);
 
-      saveToSearchHistory(city);
+    saveToSearchHistory(city);
 
-      getForecastWeather(data.coord.lat, data.coord.lon);
-    });
+    getForecastWeather(data.coord.lat, data.coord.lon);
+});
 }
 
 function getForecastWeather(lat, lon) {
-  const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`;
+    const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`;
 
-  fetch(forecastURL) 
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data); 
+    fetch(forecastURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    
 
-      forecastCardsDiv.empty(); 
+    forecastCardsDiv.empty();
 
-      const filterDates = data.list.filter((forecast) => {
+    const filterDates = data.list.filter(function(forecast) {
         return forecast.dt_txt.includes("15:00:00");
-      });
+    });
 
-      filterDates.slice(0, 5).forEach((forecast) => {
+    filterDates.slice(0, 6).forEach(function(forecast) {
         const forecastDate = new Date(forecast.dt_txt);
+    
+
         const forecastCardHTML = `
           <div class="forecast-card">
             <h3>${forecastDate.toLocaleDateString()}</h3> 
@@ -64,39 +72,28 @@ function getForecastWeather(lat, lon) {
               forecast.weather[0].icon
             }.png" alt="weather-icon">
           </div>`;
-        forecastCardsDiv.append(forecastCardHTML); 
-      });
+
+        forecastCardsDiv.append(forecastCardHTML);
+    });
     });
 }
 
 function saveToSearchHistory(city) {
-  searchHistory.push(city);
+    searchHistory.push(city);
 
-  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-  pushSearchHistory();
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    pushSearchHistory();
 }
 
 function pushSearchHistory() {
-  historyButtons.empty(); 
+    historyButtons.empty();
 
-  searchHistory.forEach((city) => {
-    const $button = $(
-      `<button type='button' class='btn btn-secondary'></button>`
-    ) 
-      .text(city) 
-      .click(() => {
-        
-        getMainWeather(city);
-      });
-    historyButtons.append($button); 
-  });
+    searchHistory.forEach(function(city) {
+        const $button = $(`<button type='button' class='btn btn-secondary'></button>`)
+        .text(city)
+        .click(function() {
+            mainWeather(city);
+        });
+        historyButtons.append($button);
+    });
 }
-
-pushSearchHistory();
-
-form.on("submit", function (event) {
-  event.preventDefault();
-
-  let city = searchInput.val();
-  getMainWeather(city); 
-});
